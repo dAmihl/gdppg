571959499c5073ca009993ec745207d4ea4b7676
dAmihl <m.egger@e-mx.at>
2019-12-30 16:16:14 +0100

    binding works

--- gdppg.cpp
+++ gdppg.cpp
@@ -2,24 +2,71 @@
 
 #include "gdppg.h"
 
-void GdPPG::add(int p_value) {
-    count += p_value;
+void GdPPG::generate_puzzle() {
+	this->currentPuzzle = this->puzzGen->generatePuzzle(this->objects, this->events, this->rules);
 }
 
-void GdPPG::reset() {
-    count = 0;
+void GdPPG::add_object(Variant objectData) {
+
+	String object_name = objectData.call("get_object_name");
+	Array states = objectData.call("get_states");
+	Dictionary transitions = objectData.call("get_transitions");
+	String default_state = objectData.call("get_default_state");
+
+	PuzzleObject *tmpObject = new PuzzleObject(object_name.ascii().get_data());
+	tmpObject->setTemplateName(object_name.ascii().get_data());
+
+	HashMap<String, PuzzleState *> stateMap;
+	PuzzleList<PuzzleState *>::Type statesList;
+
+	for (int i = 0; i < states.size(); i++) {
+		String stateName = states[i];
+		PuzzleState *tmpState = new PuzzleState(stateName.ascii().get_data());
+		stateMap.set(stateName, tmpState);
+		statesList.push_back(tmpState);
+	}
+
+	StateTransition *stateTransition = new StateTransition();
+
+	for (int j = 0; j < transitions.size(); j++) {
+		String event_name = transitions.get_key_at_index(j);
+		Array state_pair = transitions.get_value_at_index(j);
+
+		String left_state = state_pair.get(0);
+		String right_state = state_pair.get(1);
+
+		PuzzleState *tmpS1 = stateMap.get(left_state);
+		PuzzleState *tmpS2 = stateMap.get(right_state);
+
+		stateTransition->addTransition(event_name.ascii().get_data(), *tmpS1, *tmpS2);
+
+		PuzzleEvent *tmpEvent = new PuzzleEvent(event_name.ascii().get_data(), tmpObject);
+		this->events.push_back(tmpEvent);
+	}
+	tmpObject->setStateTransition(*stateTransition);
+
+	PuzzleState *st = stateMap.get(default_state);
+	// First state in array is default state
+	tmpObject->setCurrentState(*st);
+
+	this->objects.push_back(tmpObject);
 }
 
-int GdPPG::get_total() const {
-    return count;
+String GdPPG::get_puzzle_textual_representation() {
+	return String(this->currentPuzzle->getExtendedTextualRepresentation().c_str());
 }
 
 void GdPPG::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("add", "value"), &GdPPG::add);
-    ClassDB::bind_method(D_METHOD("reset"), &GdPPG::reset);
-    ClassDB::bind_method(D_METHOD("get_total"), &GdPPG::get_total);
+	ClassDB::bind_method(D_METHOD("add_object", "ppg_object_data"), &GdPPG::add_object);
+	ClassDB::bind_method(D_METHOD("generate_puzzle"), &GdPPG::generate_puzzle);
+	ClassDB::bind_method(D_METHOD("get_puzzle_textual_representation"), &GdPPG::get_puzzle_textual_representation);
 }
 
 GdPPG::GdPPG() {
-    count = 0;
+	this->initGdPPG();
+}
+
+void GdPPG::initGdPPG() {
+
+	this->puzzGen = new PuzzleGenerator();
 }
--- gdppg.h
+++ gdppg.h
@@ -4,21 +4,28 @@
 #define GdPPG_H
 
 #include "core/reference.h"
+#include <PuzzGen.h>
 
 class GdPPG : public Reference {
     GDCLASS(GdPPG, Reference);
 
-    int count;
+	void initGdPPG();
+
+	Puzzle *currentPuzzle;
+	PuzzleGenerator *puzzGen;
+	T_PuzzleObjectList objects;
+	T_PuzzleEventList events;
+	T_PuzzleRuleList rules;
 
 protected:
     static void _bind_methods();
 
 public:
-    void add(int p_value);
-    void reset();
-    int get_total() const;
+	void generate_puzzle();
+	void add_object(Variant objectData);
+	String get_puzzle_textual_representation();
 
     GdPPG();
 };
 
-#endif // PPG_H
+#endif // GdPPG_H
